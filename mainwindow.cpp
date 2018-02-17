ec1c629d45e9d0633dcc33fe9ea53b4da9a8b0d8
C J v Velthoven <cjv.velthoven@gmail.com>
2018-02-17 21:10:07 +0100

    Database connect added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,27 @@
+//---------------------------------------------------------------------------------------
+//  FirewallLogReader
+//
+//  mainwindow.cpp
+//
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+
+#include <QtSql>
+
+#include <QtDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+//---------------------------------------------------------------------------------------
+//
+// Open database
+//
+    QSqlDatabase LogDb = QSqlDatabase::addDatabase("QPSQL");
+    LogDb.setDatabaseName("vmdevdb01");
+    if (!LogDb.open())
+        qDebug() << LogDb.lastError();
     ui->setupUi(this);
 }
 
